99abf181bacfa513a0190d6f3423b972f1b7d683
Your Name <you@example.com>
2020-03-27 21:46:36 +0000

    file to compile

---
+++ build.sh
@@ -0,0 +1,2 @@
+gcc lsa.c client/include/uxr/client/core/session/session.h client/include/uxr/client/core/session/write_access.h client/include/uxr/client/core/session/read_access.h client/include/uxr/client/core/session/create_entities_ref.h client/include/uxr/client/core/session/create_entities_xml.h client/include/uxr/client/transport.h client/include/uxr/Deployment/HelloWorld.c -lmicrocdr -lmicroxrcedds_client
+
